4e0756abaff36e69ff8fc8af56cf01a7fcd88e03
nas <nash18xy@gmail.com>
2018-10-15 11:11:19 +0800

    baru

---
+++ app/src/main/java/com/ninebyte/nas/t0/TabAdapter.java
@@ -0,0 +1,22 @@
+package com.ninebyte.nas.t0;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
